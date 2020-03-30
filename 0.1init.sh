057f3e497c65ce1449f4b0d16c0945e099cdb2d1
acer <876469033@qq.com>
2020-03-30 22:35:07 +0800

    modified docker.sh

--- 0.1init.sh
+++ 0.1init.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-
-dir_name=$(pwd)
-
-#hosts
-cat $dir_name/ip.txt >> /etc/hosts
-
 #diable firewalld
 systemctl disable firewalld
 systemctl stop firewalld
 systemctl status firewalld
 
-# data
-mkdir /data
-
 # disable selinux
-getenforce 0
+setenforce 0
 getenforce
 sed -i s/SELINUX=enforcing/SELINUX=disabled/g /etc/selinux/config 
 
@@ -24,37 +15,33 @@ cat >>/etc/security/limits.conf<<EOF
 * hard nofile 204800
 * soft nproc 204800
 * hard nproc 204800
+$(whoami) soft nofile 204800
+$(whoami) hard nofile 204800
+$(whoami) soft nproc 204800
+$(whoami) hard nproc 204800
 EOF
+
 
 #vim
 echo "alias vi=vim" >>/etc/profile
 echo "alias vi=vim" >>/root/.bashrc
 
-#rpm
-mkdir /etc/yum.repos.d/back
-mv  /etc/yum.repos.d/* mkdir /etc/yum.repos.d/back/
-rpm -ivh /root/Installation/rpm/deltarpm-3.6-3.el7.x86_64.rpm  
-rpm -ivh /root/Installation/rpm/python-deltarpm-3.6-3.el7.x86_64.rpm   
-rpm -ivh /root/Installation/rpm/createrepo-0.9.9-28.el7.noarch.rpm
-/usr/bin/createrepo -v $dir_name/rpm/
+#yum
+cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
+curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+#wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 
-cat>/etc/yum.repos.d/seye.repo<<EOF
-[seye]
-name=seye
-baseurl=file://$dir_name/rpm/
-enabled=1
-gpgcheck=0
-EOF
+wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
 
 #yum install
-#yum install epel-release -y
-yum install vim wget curl telnet nmap tcpdump unzip htop -y
+yum install epel-release bash-completion -y
+yum install vim wget axel curl telnet nmap tcpdump unzip htop ntp lrzsz -y
+
+timedatectl set-ntp yes
 
 #crontab
-[ `rpm -qa ntpdate` ] || yum install ntpdate -y
+# [ `rpm -qa ntpdate` ] || yum install ntpdate -y
 
-echo "*/50 * * * * /sbin/ntpdate ntp1.aliyun.com" >>/var/spool/cron/root
-
-/sbin/ntpdate ntp1.aliyun.com
+# echo "*/50 * * * * /sbin/ntpdate ntp1.aliyun.com" >>/var/spool/cron/root
 
 hwclock -w
--- 0.2docker.sh
+++ 0.2docker.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 # install docker
-#wget -O /etc/yum.repos.d/docker-ce.repo  https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-
 #-----------------------------
-yum clean all
-yum makecache 
-yum install docker-ce -y
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+yum makecache fast	
+yum -y install docker-ce
 docker -v
 systemctl enable docker
 systemctl start docker
-\cp ./images-conf/daemon.json  /etc/docker/
-systemctl  daemon-reload
-systemctl stop docker
-systemctl start docker
+cat >/etc/docker/daemon.json <<EOF
+{
+	"registry-mirrors": ["https://6bsrrf2m.mirror.aliyuncs.com"]
+}
+EOF
+systemctl daemon-reload
+systemctl restart docker
 systemctl status docker
 
 
